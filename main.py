cdbf9faed697c18544e7bdc0fb9188b91775dea5
Ebrahim Elgazar <ebrahimjrr@gmail.com>
2025-02-08 21:35:36 +0200

    Let's do it!!

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import os
 from fastapi import FastAPI
+<<<<<<< Updated upstream
 from routes import base
+=======
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
+>>>>>>> Stashed changes
 
 app = FastAPI()
 
